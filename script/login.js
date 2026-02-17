14002018efb2b3d0802910be9875051b7fb796f7
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-02-18 00:45:22 +0600

    login page functionality added

--- script/login.js
+++ script/login.js
@@ -1,1 +1,18 @@
 console.log('login');
+document.getElementById('login-btn').addEventListener('click',function () {
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+    
+    const inputPin = document.getElementById('input-pin');
+    const pin =inputPin.value;
+
+    if (contactNumber == "01234567890" && pin =="1234") {
+        alert('Login Success!');
+        // window.location.replace("/home.html");
+        window.location.assign("/home.html");
+    } else {
+        alert('Login Failed!');
+        return;
+    }
+})
